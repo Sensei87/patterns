22115c3a596489ea34589bd106d28235558b283c
Sergey Povarchuk <s.povarchuk@gmail.com>
2020-06-24 07:48:47 +0300

    added pattern Command

---
+++ src/com/pov/behavioral_patterns/command/Light.java
@@ -0,0 +1,10 @@
+package com.pov.behavioral_patterns.command;
+
+public class Light {
+    public void turnOn(){
+        System.out.println("The light is on ");
+    }
+    public void turnOff(){
+        System.out.println("The light is off ");
+    }
+}
---
+++ src/com/pov/behavioral_patterns/command/SwitchOffCommand.java
@@ -0,0 +1,15 @@
+package com.pov.behavioral_patterns.command;
+
+public class SwitchOffCommand implements Command {
+
+    private final Light light;
+
+    public SwitchOffCommand(Light light) {
+        this.light = light;
+    }
+
+    @Override
+    public void execute() {
+        light.turnOff();
+    }
+}
